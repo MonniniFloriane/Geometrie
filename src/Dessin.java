32bbd1304bc8a716b81eeb4aa476891645518b6b
dev_flo <florianemonnini@gmail.com>
2025-01-24 11:48:31 +0100

    perimetre dessin

--- src/Dessin.java
+++ src/Dessin.java
@@ -3,23 +3,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Dessin {
-    private List<Forme> formes = new ArrayList<>();
+    private final List<Forme> formes = new ArrayList<>();
 
     public Dessin(Forme...formes) {
         this.formes.addAll(Arrays.asList(formes));
     }
 
-    public Forme getForme() {
-        return formes.getFirst();
-    }
-
-    public void setForme(List<Forme> forme) {
-        this.formes = forme;
+    public double getPerimetre() {
+        int total = 0;
+        for (Forme forme : this.formes) {
+            total += (int) forme.getPerimetre();
+        }
+        return total;
     }
 
     @Override
     public String toString() {
         return "Je suis un dessin qui contient \n"
-                + this.formes + "\n";
+                + this.formes + "\n"
+                + "je messure" + " " + getPerimetre();
     }
 }
